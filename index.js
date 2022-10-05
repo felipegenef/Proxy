4db5122d872eb7426663eb8ea77db4c450826f61
Felipe Gené <felipegenef@gmail.com>
2022-10-04 21:46:27 -0300

    second request

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-
+app.use(express.json());
 app.post("/", async (req, res) => {
   try {
     const body = req.body;
-
+    console.log("Making request");
     const response = await axios.post(body.url, body);
     res.status(200).json(response.data);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "errror on making request" });
   }
 });
